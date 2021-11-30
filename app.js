81484901c89bb33054caf55fd181ec12dc6c265f
Lyudmila <milo4kamoya@gmail.com>
2021-11-29 17:11:29 -0800

    DataBase Applied

--- app.js
+++ app.js
@@ -1,16 +1,67 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: String,
+    password: String
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
 
 app.get("/", (req, res) => {
-  res.send("Hello...")
+  res.render("home");
 })
+
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+app.post("/register", (req, res) => {
+  const newUser = new User ({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+})
+
+app.post("/login", (req, res) => {
+  const userName = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: userName}, function(err, foundUser) {
+    if(err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 
 
 
